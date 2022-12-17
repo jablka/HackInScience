2cde309387d74085edf789d03a0b054ec636fced
Marian Pocitac <mrnkpst@gmail.com>
2022-12-17 18:46:36 +0100

    added

---
+++ Python - Basics/10 Comparisons, version2.py
@@ -0,0 +1,22 @@
+the_list = [
+    143266561,
+    1738152473,
+    312377936,
+    1027708881,
+    1871655963,
+    1495785517,
+    1858250798,
+    1693786723,
+    374455497,
+    430158267,
+]
+
+
+maximum = the_list[0]
+for i in the_list:
+    if i > maximum:
+        maximum = i
+    
+print(maximum)
+
+# print(max(the_list))
